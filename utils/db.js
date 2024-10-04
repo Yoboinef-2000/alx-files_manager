f7828810d33677fe4668b9531443021102bc4b9d
Yoboinef-2000 <135835812+Yoboinef-2000@users.noreply.github.com>
2024-10-04 15:41:58 -0400

    Simple Files Manager.

--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ class DBClient {
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}`;
-    
+
     this.client = new MongoClient(url, { useUnifiedTopology: true });
     this.client.connect().then(() => {
       this.db = this.client.db(database);
